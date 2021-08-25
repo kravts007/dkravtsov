a8f7d5c3b2431753b584a8da7b2c63d6e2ab8116
Dmytro Kravtsov <kravts0@gmail.com>
2021-08-25 14:28:58 +0300

    Add program which calculate factorial of number with while loop

---
+++ src/homework4/NFactorialWhile_3.java
@@ -0,0 +1,23 @@
+package homework4;
+
+import java.util.Scanner;
+
+public class NFactorialWhile_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for which we will be looking for the factorial:");
+        int n = sc.nextInt();
+
+        int nFactorial = 1;
+        int count = 1;
+        if (n < 0) {
+            System.out.println("You entered incorrect number");
+        } else {
+            while (count <= n) {
+                nFactorial *= count;
+                count++;
+            }
+            System.out.println("Factorial of " + n + " is " + nFactorial);
+        }
+    }
+}
