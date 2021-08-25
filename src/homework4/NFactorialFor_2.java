d3c6da4d7796b4222ddb072a46f6314afb350b21
Dmytro Kravtsov <kravts0@gmail.com>
2021-08-25 14:28:48 +0300

    Add program which calculate factorial of number with for loop

---
+++ src/homework4/NFactorialFor_2.java
@@ -0,0 +1,22 @@
+package homework4;
+
+import java.util.Scanner;
+
+public class NFactorialFor_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for which we will be looking for the factorial:");
+        int n = sc.nextInt();
+
+        if (n >= 0) {
+            int nFactorial = 1;
+            for (int i = 1; i <= n; i++) {
+                nFactorial *= i;
+            }
+            System.out.println("Factorial of " + n + " is " + nFactorial);
+        } else {
+            System.out.println("You entered incorrect number");
+        }
+
+    }
+}
