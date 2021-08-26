20c84d594206ea1727fe882baf46f1bdf38c9f09
Dmytro Kravtsov <kravts0@gmail.com>
2021-08-26 15:58:56 +0300

    7. Need to display the multiplication table by X: (any number entered from the console)

--- src/homework4/MultiplicationTableByX_7.java
+++ src/homework4/MultiplicationTableByX_7.java
@@ -1,20 +1,31 @@
 package homework4;
 
 import java.util.Scanner;
-
+/**
+ * Необходимо вывести на экран таблицу умножения на Х: (любое число вводимое из консоли)
+ *  НЕ СОВСЕМ ПОНЯЛ УСЛОВИЕ ЗАДАЧИ, ПОЭТОМУ СДЕЛАЛ 2 ВАРИАНТА.
+ *  ЗАКОМЕНТИРОВАЛ, КАК ПО МОЕМУ ПРАВИЛЬНО.
+ *
+ */
 public class MultiplicationTableByX_7 {
     public static void main(String[] args) {
+        //read data source
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите первое число ");
+        System.out.println("Enter the number ");
         int x = scanner.nextInt();
-        System.out.println("Введите второе число");
-        int y = scanner.nextInt();
+//        System.out.println("Enter second number ");
+//        int y = scanner.nextInt();
 
-        for (int i = 1; i <= x; i++) {
-            for (int j = 1; j <= y; j++) {
-                System.out.print(i + "x" + j + "=" + (i * j) + "\t");
-            }
-            System.out.println();
+//        for (int i = 1; i <= 10; i++) {
+//            for (int j = 1; j <= x; j++) {
+//                System.out.print(i + "x" + j + "=" + (i * j) + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        //processing + display result
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "x" + x + "=" + (i * x) + "\t");
         }
     }
 }
