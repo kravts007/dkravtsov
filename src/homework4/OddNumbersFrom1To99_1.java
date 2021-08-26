5b5af0b212a43dcc4daf2854a1e51925b852cd2f
Dmytro Kravtsov <kravts0@gmail.com>
2021-08-26 16:09:48 +0300

    1. Use a For loop to display the odd numbers from 1 to 99.

--- src/homework4/OddNumbersFrom1To99_1.java
+++ src/homework4/OddNumbersFrom1To99_1.java
@@ -5,7 +5,7 @@ package homework4;
  */
 public class OddNumbersFrom1To99_1 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= 99; i++) {
             if (i % 2 != 0) {
                 System.out.print(i + " ");
             }
