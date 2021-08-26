e5fd05b89b4585236737c767bc6debb8fb228093
Dmytro Kravtsov <kravts0@gmail.com>
2021-08-26 15:43:52 +0300

    5. Print the first 10 numbers of the sequence 0, -5, -10, -15...

--- src/homework4/DisplayFirst10NumbersFromOrder_6.java
+++ src/homework4/DisplayFirst10NumbersFromOrder_6.java
@@ -1,23 +1,20 @@
 package homework4;
 
+/**
+ * Вывести 10 первых чисел последовательности 0, -5,-10,-15...
+ */
 public class DisplayFirst10NumbersFromOrder_6 {
     public static void main(String[] args) {
-
-//        for (int i = 0; i >= -50; i --) {
-//            if (i % 5 == 0) {
-//                System.out.print(i + ", ");
-//                if (i == -45) {
-//                    System.out.print(i);
-//                }
-//            }
-//        }
         int count = 1;
         int i = 5;
         do {
             i -= 5;
-            System.out.print(i + ", ");
             count++;
-
+            if (count <= 10) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.print(i);
+            }
         } while (count <= 10);
 
     }
