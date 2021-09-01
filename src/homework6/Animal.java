bc6388b8dcc63d9887864631a0637c05880413ae
Dmytro Kravtsov <kravts0@gmail.com>
2021-09-01 22:30:46 +0300

    Add general class Animal

---
+++ src/homework6/Animal.java
@@ -0,0 +1,53 @@
+package homework6;
+
+public class Animal {
+
+    private int id;
+    private int age;
+    private int weight;
+    private String color;
+
+
+    public Animal(int id, int age, int weight, String color) {
+        this.id = id;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String voice() {
+        return "Hello, ";
+    }
+}
