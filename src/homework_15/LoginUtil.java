5c8fb138ae7ab5c430101a1e2bc3d4243e4b1020
Dmytro Kravtsov <kravts0@gmail.com>
2021-10-11 14:54:03 +0300

    Homework 15 trows Exceptions

--- src/homework_15/LoginUtil.java
+++ src/homework_15/LoginUtil.java
@@ -9,35 +9,36 @@ public class LoginUtil {
     static {
         loginData.put("WhiteOcean", "bigFish");
         loginData.put("Drakula", "nosferatu");
+        loginData.put("Ivan", "Ivanov05");
+        loginData.put("Petr7", "Petrov7");
+        loginData.put("Captain51", "America");
+        loginData.put("noob", "nagibator2005");
     }
 
     static boolean isUserAuthentic(String login, String password, String confirmPassword) {
-        if (login != null && password != null) {
-            if (password.equals(confirmPassword)) {
-                return true;
-            }
-        }
-        if (loginData.containsKey(login)) {
-            return true;
-        }
         try {
+            assert password != null;
+            if (!password.equals(confirmPassword)) throw new WrongPasswordException();
+            assert login != null;
             isLoginValid(login);
             isPasswordValid(password);
         } catch (WrongLoginException | WrongPasswordException exception) {
             return false;
         }
-
-        return false;
+        return true;
     }
 
     private static void isLoginValid(String login) throws WrongLoginException {
-        if (!(login.length() > 0 && login.length() < 20) || !login.matches("^[a-zA-Z0-9]+$")) {
+        if (!(login.length() > 0 && login.length() < 20)
+                || !login.matches("^[a-zA-Z0-9|_]+$")
+                || loginData.containsKey(login)) {
             throw new WrongLoginException("Login is incorrect");
         }
     }
 
     private static void isPasswordValid(String password) throws WrongPasswordException {
-        if (!(password.length() < 20) || (!password.matches("^[a-zA-Z0-9]+$"))) {
+        if (!(password.length() < 20) || (!password.matches("^[a-zA-Z0-9|_]+$"))
+        ) {
             throw new WrongPasswordException("Password is incorrect");
         }
     }
--- src/homework_15/TestLoginUtil.java
+++ src/homework_15/TestLoginUtil.java
@@ -2,6 +2,6 @@ package homework_15;
 
 public class TestLoginUtil {
     public static void main(String[] args) {
-        System.out.println(LoginUtil.isUserAuthentic("WhiteOcean", "bigFish","bigFish"));
+        System.out.println(LoginUtil.isUserAuthentic("noob_2005", "nagibator2005","nagibator2005"));
     }
 }
